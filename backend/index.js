c674cb08f47ea2e556072507b59ac137410c2260
Nitin Kumar <your-email@example.com>
2026-07-29 10:38:58 +0530

    commit 3

--- backend/index.js
+++ backend/index.js
@@ -16,15 +16,29 @@ const PORT = process.env.PORT || 5000;
  
 
 const app = express();
+
+// Serve frontend in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  
+  app.use((req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('ShopNest API is running in Development mode...');
+  });
+}
+
 app.use(cors({
   origin: process.env.FRONTEND_URL || 'http://localhost:5174',
   credentials: true, // Allow cookies to be sent
 }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
 
 // Use the authentication routes
 app.use('/api/auth', require('./routes/authRoutes'));
